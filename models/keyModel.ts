04ba2295eaf1459f07d3e114e711583ffb445731
LordPrinz <plazinski2902@gmail.com>
2023-03-18 23:18:19 +0100

    Add keyModel

---
+++ models/keyModel.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const keyModel = new mongoose.Schema({
+	currentOwner: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		default: null,
+	},
+	cardIds: [
+		{
+			type: Number,
+		},
+	],
+});
+
+keyModel.virtual("room", {
+	ref: "Room",
+	foreignField: "key",
+	localField: "_id",
+});
+
+keyModel.virtual("id").get(function () {
+	return this._id.toHexString();
+});
+
+keyModel.set("toJSON", {
+	virtuals: true,
+	versionKey: false,
+});
