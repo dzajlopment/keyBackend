09922bc232ee0cf23127b344686e5241751361f1
LordPrinz <plazinski2902@gmail.com>
2023-03-29 00:04:21 +0200

    Implement delete all function

---
+++ dev-data/data/deleteData.ts
@@ -0,0 +1,11 @@
+import { KeyModel } from "../../models/keyModel";
+import { RentHistoryModel } from "../../models/rentHistoryModel";
+import { RoomModel } from "../../models/roomModel";
+import { UserModel } from "../../models/userModel";
+
+export default async () => {
+	await RoomModel.deleteMany({});
+	await KeyModel.deleteMany({});
+	await UserModel.deleteMany({});
+	await RentHistoryModel.deleteMany({});
+};
